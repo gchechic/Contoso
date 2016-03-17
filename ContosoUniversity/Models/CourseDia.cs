0bd32ee2c30ea71241216fbb43e82ea25c539b70
gchechic <gchechic@yahoo.com>
2016-03-17 10:40:21 -0300

    Primero

---
+++ ContosoUniversity/Models/CourseDia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class CourseDia
+    {
+        public int ID { get;set; }
+        public int CourseID { get; set; }
+        public int Dia { get; set; }
+    }
+}
---
+++ ContosoUniversity/Models/Feriado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Feriado
+    {
+        public int ID {get;set;}
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; }
+        public bool x { get; set; }
+    }
+}
